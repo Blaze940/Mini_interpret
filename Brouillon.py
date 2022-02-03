65d2301e2727d5d6a253f2db2a5c73ff509ed838
Blz <moustapha.diarra@inetum.com>
2022-02-03 12:22:34 +0100

    Version correcte du brouillon.py

--- Brouillon.py
+++ Brouillon.py
@@ -12,7 +12,7 @@ reserved = {
 tokens = [
     'NUMBER','MINUS',
     'PLUS','TIMES','DIVIDE',
-    'LPAREN','RPAREN', 'AND', 'OR','TRUE','FALSE', 'SEMICOLON','NAME','AFFECT', 'INFTO', 'SUPTO'
+    'LPAREN','RPAREN', 'AND', 'OR','TRUE','FALSE', 'SEMICOLON','NAME','AFFECT', 'INFTO', 'SUPTO','SAME'
     ] + list(reserved.values())
 # Tokens
 t_SEMICOLON = r'\;'
@@ -29,6 +29,7 @@ t_FALSE   = r'F'
 t_AFFECT  = r'='
 t_INFTO   = r'\<'
 t_SUPTO   = r'\>'
+t_SAME    = r'=='
 names = {}
 tup = (('*', ('+', 1, 2),3))
 
@@ -67,50 +68,49 @@ precedence = (
     )
 
 def p_start(p):
-    'START : expression'
+    'START : BLOC'
     p[0] = ('START',p[1])
     print('Arbre de dérivation = ',p[0])
     printTreeGraph(p[0])
-    print('Calc> ',eval(p[0]))
+    evalInst(p[1])
 
 def p_bloc(p):
-    ''' START : START statement SEMICOLON
+    ''' BLOC : BLOC statement SEMICOLON
            | statement SEMICOLON '''
-    p[0] = ('START', p[1])
-    p[0] = p[1]
-    print(p[0])
-    printTreeGraph(p[0])
+    if len(p) == 4 :
+        p[0] = ('bloc',p[1], p[2])
+    else:
+        p[0] = ('bloc',p[1], 'empty')
 
 def p_statement_print(p):
     'statement : PRINT LPAREN expression RPAREN'
     p[0] = ('print', p[3])
-    print(p[3])
 
 def p_statement_affect(p):
     'statement : NAME AFFECT expression '
-    p[0] = ('assign', p[1], '=', p[3])
+    p[0] = ('assign', p[1], p[3])
 
 def p_expression_binop_plus(p):
     'expression : expression PLUS expression'
-    p[0] = ('Expr', p[1], '+', p[3])
+    p[0] = ('+', p[1], p[3])
 
 def p_expression_binop_times(p):
     'expression : expression TIMES expression'
-    p[0] = ('Expr', p[1], '*', p[3])
+    p[0] = ('*', p[1], p[3])
 
 def p_expression_binop_divide_and_minus(p):
     '''expression : expression MINUS expression
 				| expression DIVIDE expression'''
-    if p[2] == '-': p[0] = ('Expr', p[1], '-', p[3])
-    else : p[0] = ('Expr', p[1], '/', p[3])
+    if p[2] == '-': p[0] = ('-', p[1], p[3])
+    else : p[0] = ('/', p[1], p[3])
 
 def p_expression_group(p):
     'expression : LPAREN expression RPAREN'
-    p[0] = ('Expr', '(', p[2], ')')
+    p[0] = p[2]
 
 def p_expression_number(p):
     'expression : NUMBER'
-    p[0] =  p[1]
+    p[0] = p[1]
 
 def p_expression_name(p):
     'expression : NAME'
@@ -136,14 +136,26 @@ def p_expression_inequal(p) :
     '''expression : expression INFTO expression
                     | expression SUPTO expression '''
     if p[2] == '<':
-        p[0] = ('Expr', p[1], '<', p[3])
+        p[0] = ('<',p[1],p[3])
     elif p[2] == '>':
-        p[0] = ('Expr', p[1], '>', p[3])
+        p[0] = ('>',p[1],p[3])
 
 def p_error(p):
     print("Syntax error at '%s'" % p.value)
 
-def eval(t) :
+def evalInst(t) :
+    print(' evalInst de ', t)
+    if t == 'empty': return
+    elif t[0] == 'bloc':
+        evalInst(t[1])
+        evalInst(t[2])
+    elif t[0] == 'assign':
+        names[t[1]] = eval(t[2])
+    elif t[0] == 'print':
+        print('Calc >>',eval(t[1]))
+
+
+def eval(t) : #evalExpre
     print(' eval de ', t)
     if type(t) is int:
         return t
@@ -166,14 +178,14 @@ def eval(t) :
             return eval(t[1]) < eval(t[2])
         elif t[0] == '>':
             return eval(t[1]) > eval(t[2])
-        elif t[0] == '=':
-            return eval(t[2])
+        elif t[0] == '==':
+            return eval(t[1]) == eval(t[2])
 
 
 
 import ply.yacc as yacc
 yacc.yacc()
-s = 'a=2;print(a);'
+s = ' a=2 ; print(a) ; '
 #s = input('calc > ')
 #s = ' eval(tup) ; '
 yacc.parse(s)
--- parsetab.py
+++ parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftANDleftORnonassocINFTOSUPTOAFFECTleftPLUSMINUSleftTIMESDIVIDENUMBER MINUS PLUS TIMES DIVIDE LPAREN RPAREN AND OR TRUE FALSE SEMICOLON NAME AFFECT INFTO SUPTO PRINTSTART : expression START : START statement SEMICOLON\n           | statement SEMICOLON statement : PRINT LPAREN expression RPARENstatement : NAME AFFECT expression expression : expression PLUS expressionexpression : expression TIMES expressionexpression : expression MINUS expression\n\t\t\t\t| expression DIVIDE expressionexpression : LPAREN expression RPARENexpression : NUMBERexpression : NAMEexpression : TRUEexpression : FALSEexpression : expression AND expression\n                | expression OR expressionexpression : expression INFTO expression\n                    | expression SUPTO expression '
+_lr_signature = 'leftANDleftORnonassocINFTOSUPTOAFFECTleftPLUSMINUSleftTIMESDIVIDENUMBER MINUS PLUS TIMES DIVIDE LPAREN RPAREN AND OR TRUE FALSE SEMICOLON NAME AFFECT INFTO SUPTO SAME PRINTSTART : BLOC BLOC : BLOC statement SEMICOLON\n           | statement SEMICOLON statement : PRINT LPAREN expression RPARENstatement : NAME AFFECT expression expression : expression PLUS expressionexpression : expression TIMES expressionexpression : expression MINUS expression\n\t\t\t\t| expression DIVIDE expressionexpression : LPAREN expression RPARENexpression : NUMBERexpression : NAMEexpression : TRUEexpression : FALSEexpression : expression AND expression\n                | expression OR expressionexpression : expression INFTO expression\n                    | expression SUPTO expression '
     
-_lr_action_items = {'LPAREN':([0,4,9,12,13,14,15,16,17,18,19,23,24,],[4,4,24,4,4,4,4,4,4,4,4,4,4,]),'NUMBER':([0,4,12,13,14,15,16,17,18,19,23,24,],[5,5,5,5,5,5,5,5,5,5,5,5,]),'NAME':([0,1,2,4,5,6,7,8,12,13,14,15,16,17,18,19,20,22,23,24,25,26,27,28,29,30,31,32,33,34,],[6,11,-1,22,-11,-12,-13,-14,22,22,22,22,22,22,22,22,-3,-12,22,22,-2,-6,-7,-8,-9,-15,-16,-17,-18,-10,]),'TRUE':([0,4,12,13,14,15,16,17,18,19,23,24,],[7,7,7,7,7,7,7,7,7,7,7,7,]),'FALSE':([0,4,12,13,14,15,16,17,18,19,23,24,],[8,8,8,8,8,8,8,8,8,8,8,8,]),'PRINT':([0,1,2,5,6,7,8,20,22,25,26,27,28,29,30,31,32,33,34,],[9,9,-1,-11,-12,-13,-14,-3,-12,-2,-6,-7,-8,-9,-15,-16,-17,-18,-10,]),'$end':([1,2,5,6,7,8,20,22,25,26,27,28,29,30,31,32,33,34,],[0,-1,-11,-12,-13,-14,-3,-12,-2,-6,-7,-8,-9,-15,-16,-17,-18,-10,]),'PLUS':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[12,-11,-12,-13,-14,12,-12,-6,-7,-8,-9,12,12,12,12,-10,12,12,]),'TIMES':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[13,-11,-12,-13,-14,13,-12,13,-7,13,-9,13,13,13,13,-10,13,13,]),'MINUS':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[14,-11,-12,-13,-14,14,-12,-6,-7,-8,-9,14,14,14,14,-10,14,14,]),'DIVIDE':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[15,-11,-12,-13,-14,15,-12,15,-7,15,-9,15,15,15,15,-10,15,15,]),'AND':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[16,-11,-12,-13,-14,16,-12,-6,-7,-8,-9,-15,-16,-17,-18,-10,16,16,]),'OR':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[17,-11,-12,-13,-14,17,-12,-6,-7,-8,-9,17,-16,-17,-18,-10,17,17,]),'INFTO':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[18,-11,-12,-13,-14,18,-12,-6,-7,-8,-9,18,18,None,None,-10,18,18,]),'SUPTO':([2,5,6,7,8,21,22,26,27,28,29,30,31,32,33,34,35,36,],[19,-11,-12,-13,-14,19,-12,-6,-7,-8,-9,19,19,None,None,-10,19,19,]),'SEMICOLON':([3,5,7,8,10,22,26,27,28,29,30,31,32,33,34,35,37,],[20,-11,-13,-14,25,-12,-6,-7,-8,-9,-15,-16,-17,-18,-10,-5,-4,]),'RPAREN':([5,7,8,21,22,26,27,28,29,30,31,32,33,34,36,],[-11,-13,-14,34,-12,-6,-7,-8,-9,-15,-16,-17,-18,-10,37,]),'AFFECT':([6,11,],[23,23,]),}
+_lr_action_items = {'PRINT':([0,2,7,10,],[4,4,-3,-2,]),'NAME':([0,2,7,8,9,10,11,20,21,22,23,24,25,26,27,],[5,5,-3,14,14,-2,14,14,14,14,14,14,14,14,14,]),'$end':([1,2,7,10,],[0,-1,-3,-2,]),'SEMICOLON':([3,6,13,14,15,16,17,19,28,29,30,31,32,33,34,35,36,],[7,10,-11,-12,-13,-14,-5,-4,-10,-6,-7,-8,-9,-15,-16,-17,-18,]),'LPAREN':([4,8,9,11,20,21,22,23,24,25,26,27,],[8,11,11,11,11,11,11,11,11,11,11,11,]),'AFFECT':([5,],[9,]),'NUMBER':([8,9,11,20,21,22,23,24,25,26,27,],[13,13,13,13,13,13,13,13,13,13,13,]),'TRUE':([8,9,11,20,21,22,23,24,25,26,27,],[15,15,15,15,15,15,15,15,15,15,15,]),'FALSE':([8,9,11,20,21,22,23,24,25,26,27,],[16,16,16,16,16,16,16,16,16,16,16,]),'RPAREN':([12,13,14,15,16,18,28,29,30,31,32,33,34,35,36,],[19,-11,-12,-13,-14,28,-10,-6,-7,-8,-9,-15,-16,-17,-18,]),'PLUS':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[20,-11,-12,-13,-14,20,20,-10,-6,-7,-8,-9,20,20,20,20,]),'TIMES':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[21,-11,-12,-13,-14,21,21,-10,21,-7,21,-9,21,21,21,21,]),'MINUS':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[22,-11,-12,-13,-14,22,22,-10,-6,-7,-8,-9,22,22,22,22,]),'DIVIDE':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[23,-11,-12,-13,-14,23,23,-10,23,-7,23,-9,23,23,23,23,]),'AND':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[24,-11,-12,-13,-14,24,24,-10,-6,-7,-8,-9,-15,-16,-17,-18,]),'OR':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[25,-11,-12,-13,-14,25,25,-10,-6,-7,-8,-9,25,-16,-17,-18,]),'INFTO':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[26,-11,-12,-13,-14,26,26,-10,-6,-7,-8,-9,26,26,None,None,]),'SUPTO':([12,13,14,15,16,17,18,28,29,30,31,32,33,34,35,36,],[27,-11,-12,-13,-14,27,27,-10,-6,-7,-8,-9,27,27,None,None,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'START':([0,],[1,]),'expression':([0,4,12,13,14,15,16,17,18,19,23,24,],[2,21,26,27,28,29,30,31,32,33,35,36,]),'statement':([0,1,],[3,10,]),}
+_lr_goto_items = {'START':([0,],[1,]),'BLOC':([0,],[2,]),'statement':([0,2,],[3,6,]),'expression':([8,9,11,20,21,22,23,24,25,26,27,],[12,17,18,29,30,31,32,33,34,35,36,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -26,22 +26,22 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> START","S'",1,None,None,None),
-  ('START -> expression','START',1,'p_start','Brouillon.py',70),
-  ('START -> START statement SEMICOLON','START',3,'p_bloc','Brouillon.py',77),
-  ('START -> statement SEMICOLON','START',2,'p_bloc','Brouillon.py',78),
-  ('statement -> PRINT LPAREN expression RPAREN','statement',4,'p_statement_print','Brouillon.py',82),
-  ('statement -> NAME AFFECT expression','statement',3,'p_statement_affect','Brouillon.py',86),
-  ('expression -> expression PLUS expression','expression',3,'p_expression_binop_plus','Brouillon.py',90),
-  ('expression -> expression TIMES expression','expression',3,'p_expression_binop_times','Brouillon.py',94),
-  ('expression -> expression MINUS expression','expression',3,'p_expression_binop_divide_and_minus','Brouillon.py',98),
-  ('expression -> expression DIVIDE expression','expression',3,'p_expression_binop_divide_and_minus','Brouillon.py',99),
-  ('expression -> LPAREN expression RPAREN','expression',3,'p_expression_group','Brouillon.py',104),
-  ('expression -> NUMBER','expression',1,'p_expression_number','Brouillon.py',108),
-  ('expression -> NAME','expression',1,'p_expression_name','Brouillon.py',112),
-  ('expression -> TRUE','expression',1,'p_expressionTrue','Brouillon.py',116),
-  ('expression -> FALSE','expression',1,'p_expressionFalse','Brouillon.py',120),
-  ('expression -> expression AND expression','expression',3,'p_expression_binop_boop','Brouillon.py',124),
-  ('expression -> expression OR expression','expression',3,'p_expression_binop_boop','Brouillon.py',125),
-  ('expression -> expression INFTO expression','expression',3,'p_expression_inequal','Brouillon.py',132),
-  ('expression -> expression SUPTO expression','expression',3,'p_expression_inequal','Brouillon.py',133),
+  ('START -> BLOC','START',1,'p_start','Brouillon.py',71),
+  ('BLOC -> BLOC statement SEMICOLON','BLOC',3,'p_bloc','Brouillon.py',78),
+  ('BLOC -> statement SEMICOLON','BLOC',2,'p_bloc','Brouillon.py',79),
+  ('statement -> PRINT LPAREN expression RPAREN','statement',4,'p_statement_print','Brouillon.py',85),
+  ('statement -> NAME AFFECT expression','statement',3,'p_statement_affect','Brouillon.py',89),
+  ('expression -> expression PLUS expression','expression',3,'p_expression_binop_plus','Brouillon.py',93),
+  ('expression -> expression TIMES expression','expression',3,'p_expression_binop_times','Brouillon.py',97),
+  ('expression -> expression MINUS expression','expression',3,'p_expression_binop_divide_and_minus','Brouillon.py',101),
+  ('expression -> expression DIVIDE expression','expression',3,'p_expression_binop_divide_and_minus','Brouillon.py',102),
+  ('expression -> LPAREN expression RPAREN','expression',3,'p_expression_group','Brouillon.py',107),
+  ('expression -> NUMBER','expression',1,'p_expression_number','Brouillon.py',111),
+  ('expression -> NAME','expression',1,'p_expression_name','Brouillon.py',115),
+  ('expression -> TRUE','expression',1,'p_expressionTrue','Brouillon.py',119),
+  ('expression -> FALSE','expression',1,'p_expressionFalse','Brouillon.py',123),
+  ('expression -> expression AND expression','expression',3,'p_expression_binop_boop','Brouillon.py',127),
+  ('expression -> expression OR expression','expression',3,'p_expression_binop_boop','Brouillon.py',128),
+  ('expression -> expression INFTO expression','expression',3,'p_expression_inequal','Brouillon.py',135),
+  ('expression -> expression SUPTO expression','expression',3,'p_expression_inequal','Brouillon.py',136),
 ]
